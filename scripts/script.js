848d9ae7e293a58a40c2dfcc2569f5d47fd8e2ea
Tin Nguyen <powertinnguyen@gmail.com>
2025-09-12 08:49:48 +0200

    Alle studenten in klas 1i toegevoegd en ook een slider toegevoegd en stijl gegeven

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,12 @@
-
-
-
-
-let touchEvent = null;
-if ('ontouchstart' in window || navigator.maxTouchPoints) {
-  touchEvent = 'touchstart';
-} else {
-  touchEvent = 'click';
-}
-document.querySelectorAll('.member').forEach(member => {
-  member.addEventListener(touchEvent, () => {
-    document.querySelectorAll('.member').forEach(m => m.classList.remove('active'));
-    member.classList.add('active');
-  });
-});
+// let touchEvent = null;
+// if ('ontouchstart' in window || navigator.maxTouchPoints) {
+//   touchEvent = 'touchstart';
+// } else {
+//   touchEvent = 'click';
+// }
+// document.querySelectorAll('.member').forEach(member => {
+//   member.addEventListener(touchEvent, () => {
+//     document.querySelectorAll('.member').forEach(m => m.classList.remove('active'));
+//     member.classList.add('active');
+//   });
+// });
